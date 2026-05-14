21d6a1fbbe5eb9b1ef3096d132b5e95609da8d7d
MATEO HERNANDEZ KRASUK <49979691@ort>
2026-05-14 08:44:56 -0300

    Receta

---
+++ Models/receta.cs
@@ -0,0 +1,103 @@
+public class receta 
+{
+public string nombreCocinero{get;set;}
+public datetime fechaDeNacimiento{get;set;}
+public string tipoComida{get;set}
+public int presupuesto{get;set;}
+public int cantidadPersonas{get;set;}
+
+public int CalcularEdad(datetime fechaDeNacimientos){
+int edad =  DateTime.Today.Year - fechaDeNacimiento.Year;
+if  (fechaDeNacimiento.Month > DateTime.Today.Month )
+{
+    edad-=
+} 
+if (fechaDeNacimiento.Month= DateTime.Today.Month && fechaDeNacimiento.Day > DateTime.Today.Day)
+{
+    edad-=
+}
+return edad
+}
+public string DeterminarPlato()
+{
+    string plato;
+    if (tipoComida == "caliente" && presupuesto<3000)
+    {
+        plato = "Fideos Con Manteca";
+        return plato;
+    }
+    if (tipoComida == "caliente" && (presupuesto<7000 && presupuesto>3000))
+    {
+        plato = "Arroz con verduras salteadas";
+        return plato;
+    }
+    if (tipoComida == "caliente" && presupuesto>7000)
+    {
+        plato = "Pollo Al horno con guarnicion";
+        return plato;
+    }
+    if (tipoComida == "fria" && presupuesto<3000)
+    {
+        plato = "ensalada simple";
+        return plato;
+    }
+    if (tipoComida == "fria" && (presupuesto>3000 && presupuesto<7000))
+    {
+        plato = "Ensalada completa con proteína";
+        return plato;
+    }
+    if (tipoComida == "fria" && presupuesto>7000)
+    {
+        plato = "Tabla de fiambres y quesos";
+        return plato;
+    }
+    
+
+}
+public int CalcularTiempo()
+{
+    int tiempoestimado;
+    if (tipoComida == "Caliente" && cantidadPersonas=1)
+    {
+        tiempoestimado = 20;
+        return tiempoestimado;
+    }
+    if (tipoComida == "Caliente" && (cantidadPersonas>=2 && cantidadPersonas<=3))
+    {
+        tiempoestimado= 20;
+        return tiempoestimado;
+    }
+    if (tipoComida == "Caliente" && (cantidadPersonas>=4 && cantidadPersonas<=7))
+    {
+        tiempoestimado = 40;
+        return tiempoestimado;
+    }
+    if (tipoComida == "Caliente" && cantidadPersonas>=8)
+    {
+        tiempoestimado = 80;
+        return tiempoestimado;
+    }  
+    if (tipoComida == "Fria" && (cantidadPersonas>=1 && cantidadPersonas<=3 ))
+    {
+        tiempoestimado = 10;
+        return tiempoestimado;
+    }
+    if (tipoComida == "Fria" && (cantidadPersonas>=2 && cantidadPersonas<=3))
+    {
+        tiempoestimado= 20;
+        return tiempoestimado;
+    }
+    if (tipoComida == "Fria" && cantidadPersonas>=4 && cantidadPersonas<=7)
+    {
+        tiempoestimado = 40;
+        return tiempoestimado;
+    }if (tipoComida == "Fria" && cantidadPersonas>=8)
+    {
+        tiempoestimado = 80;
+        return tiempoestimado;
+    }
+
+
+}
+}
+
